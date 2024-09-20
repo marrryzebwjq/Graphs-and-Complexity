bf39d712f97de928e6b67fc6813abcf0194ad594
Marie POINT <marie.point@etu.univ-nantes.fr>
2024-09-21 00:01:56 +0200

    méthodes pour print

--- src/Tools4A.java
+++ src/Tools4A.java
@@ -9,12 +9,38 @@ public class Tools4A {
     }
 
 
+    /**
+     * Affiche la matrice
+     * @param mat
+     */
     public static void printMatrix(float[][] mat) {
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j] + "   ");
             }
             System.out.println();
+        }
+    }
+
+    /**
+     * Affiche la liste d'adjacence pondérée (remarque : leurs listes ont l'air totalement mal foutues je comprends r)
+     * @param list
+     * @param length
+     */
+    public static void printAdjListW(WeightedNode4A[] list, int length) {
+        for(int i = 0; i < length; i++ ) {
+            System.out.printf("%d : %d -> %d (w : %.2f, %.2f)\n", i, list[i].getVal(), list[i].getNext().getVal(), (double)(list[i].getWeight()), (double)(list[i].getNext().getWeight()));
+        }
+    }
+
+    /**
+     * Affiche la liste d'adjacence
+     * @param list
+     * @param length
+     */
+    public static void printAdjList(Node4A[] list, int length) {
+        for(int i = 0; i < length; i++ ) {
+            System.out.printf("%d : %d -> %d", i, list[i].getVal(), list[i].getNext().getVal());
         }
     }
 }
